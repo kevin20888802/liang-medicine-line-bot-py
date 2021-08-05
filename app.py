b08ce7906d2c9ec6bdca6523b3f885593947ecbe
kevin20888802 <38619229+kevin20888802@users.noreply.github.com>
2021-08-06 04:06:55 +0800

    圖片及每秒執行
    新增接收圖片功能以及每秒執行功能

--- app.py
+++ app.py
@@ -13,10 +13,17 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+from threading import Timer
 import os
+import io
+from datetime import datetime
+from pyzbar.pyzbar import decode
+from PIL import Image
+from linebot.models.messages import ImageMessage
+from linebot.models.send_messages import ImageSendMessage
 
 #-------- Line Developer上面的bot密碼資訊 --------
-local_test = False # 自己電腦上用ngrok測試=True ; 放到Heroku上=False
+local_test = True # 自己電腦上用ngrok測試=True ; 放到Heroku上=False
 _token = "" 
 _secret = ""
 
@@ -52,18 +59,41 @@ def callback():
     return 'OK'
 pass
 
-# 當訊息傳入
+# 當文字訊息傳入
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-
+def handle_message_text(event):
     _msg = event.message.text # 傳入的訊息
-
     _return_str = "" # 傳出去的訊息
     _return_str = _msg
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=_return_str))
-
 pass
 
+# 當圖片訊息傳入
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_text(event):
+    # 取得圖片訊息(byte)並轉換成圖片形式(image)
+    value = bytearray()
+    message_content = line_bot_api.get_message_content(event.message.id)
+    for chunk in message_content.iter_content():
+        value += chunk
+    pass
+    img = Image.open(io.BytesIO(value))
+
+    # QR Code 資訊
+    print(decode(img))
+
+    #　回應
+    #line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url=_oriImgUrl,preview_image_url=_preImgUrl))
+pass
+
+# 每秒執行的放這
+def every_sec():
+    Timer(1, every_sec, []).start()
+    print("Check time : " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+pass
+every_sec()
+
+# 開啟LineBot功能
 if __name__ == "__main__":
     app.run()
 pass
